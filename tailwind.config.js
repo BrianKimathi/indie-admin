7488df73dfbc945ec43b76b7ba5d7b3ba1fe64bc
Brian Kimathi <98356481+BrianKimathi@users.noreply.github.com>
2024-12-17 13:10:03 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   darkMode: 'class', // Enable dark mode via 'class'
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
